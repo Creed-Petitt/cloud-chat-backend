3d965903f26671ea72d7857dffdb16b5cdc5eb40
Creed Petitt <creedpetitt@gmail.com>
2025-09-05 23:03:26 -0500

    initial commit

---
+++ src/main/java/com/creedpetitt/aiservicesbackend/AiServicesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.creedpetitt.aiservicesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiServicesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiServicesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/creedpetitt/aiservicesbackend/AiServicesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creedpetitt.aiservicesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServicesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
