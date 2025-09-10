d423be00215b10cb9f5f4191b0ffaf6bca0fca49
Creed Petitt <creedpetitt@gmail.com>
2025-09-10 01:06:35 -0500

    enable jpa auditing

--- src/main/java/com/creedpetitt/aiservicesbackend/AiServicesBackendApplication.java
+++ src/main/java/com/creedpetitt/aiservicesbackend/AiServicesBackendApplication.java
@@ -2,8 +2,10 @@ package com.creedpetitt.aiservicesbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AiServicesBackendApplication {
 
     public static void main(String[] args) {
