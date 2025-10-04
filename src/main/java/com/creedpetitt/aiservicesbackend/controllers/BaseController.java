8c23d3b2064d0806f70f9ef2b814555399091e69
Creed Petitt <creedpetitt@gmail.com>
2025-10-04 18:04:31 -0500

    add BaseController and RequestUtils for shared logic across controllers

---
+++ src/main/java/com/creedpetitt/aiservicesbackend/controllers/BaseController.java
@@ -0,0 +1,29 @@
+package com.creedpetitt.aiservicesbackend.controllers;
+
+import com.creedpetitt.aiservicesbackend.models.AppUser;
+import com.creedpetitt.aiservicesbackend.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.server.ResponseStatusException;
+
+public abstract class BaseController {
+
+    protected final UserService userService;
+
+    protected BaseController(UserService userService) {
+        this.userService = userService;
+    }
+
+    protected AppUser getAuthenticatedUser(Authentication authentication) {
+        String uid = authentication.getName();
+        String email = (String) authentication.getDetails();
+        return userService.getOrCreateUser(uid, email != null ? email : uid + "@firebase.user");
+    }
+
+    protected AppUser requireAuthenticatedUser(Authentication authentication) {
+        if (authentication == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authentication required");
+        }
+        return getAuthenticatedUser(authentication);
+    }
+}
---
+++ src/main/java/com/creedpetitt/aiservicesbackend/util/RequestUtils.java
@@ -0,0 +1,22 @@
+package com.creedpetitt.aiservicesbackend.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class RequestUtils {
+
+    private RequestUtils() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static String getClientIP(HttpServletRequest request) {
+        String xForwardedFor = request.getHeader("X-Forwarded-For");
+        if (xForwardedFor != null && !xForwardedFor.isEmpty()) {
+            return xForwardedFor.split(",")[0].trim();
+        }
+        String xRealIP = request.getHeader("X-Real-IP");
+        if (xRealIP != null && !xRealIP.isEmpty()) {
+            return xRealIP;
+        }
+        return request.getRemoteAddr();
+    }
+}
